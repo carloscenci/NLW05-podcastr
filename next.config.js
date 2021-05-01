016c2cad964094507d45ca7064221302db87a839
carloscenci <carlosc.estevam@gmail.com>
2021-05-01 20:35:29 -0300

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+    pwa: {
+      dest: 'public',
+      // disable: process.env.NODE_ENV === 'production',
+      register: true,
+      scope: '/app',
+      sw: 'service-worker.js',
+    },
     images : {
         domains: ['storage.googleapis.com']
     }
-}
+  })
+
+// module.exports = {
+//     images : {
+//         domains: ['storage.googleapis.com']
+//     }
+// }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+const APP_NAME = 'Podcastr'
+const APP_DESCRIPTION = 'This is an example of using next-pwa plugin in '
+
 export default class MyDocument extends Document {
     render() {
         return (
@@ -8,7 +11,21 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
                 
+                    <meta name='apple-mobile-web-app-capable' content='yes' />
+                    <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+                    <meta name='apple-mobile-web-app-title' content={APP_NAME} />
+                    <meta name='description' content={APP_DESCRIPTION} />
+                    <meta name='format-detection' content='telephone=no' />
+                    <meta name='mobile-web-app-capable' content='yes' />
+                    <link rel="icon" href="/android-chrome-64x64.png" sizes="64x64" />
+                    <link rel="icon" href="/android-chrome-192x192.png" sizes="192x192" />
+                    <link rel="icon" href="/android-chrome-512x512.png" sizes="512x512" />
+                    <link rel='manifest' href='manifest.json' />
                     <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+                    <script
+                        async
+                        src="https://unpkg.com/pwacompat"
+                        crossOrigin="anonymous"></script>
                     
                 </Head>
 
